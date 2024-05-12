5d057e13b54c0322220d12b5434b169d8fbb85da
Nexumkill <76457940+blaumath@users.noreply.github.com>
2024-05-12 17:19:57 -0300

    ops

--- src/functions/trackChecker/trackChecker.js
+++ src/functions/trackChecker/trackChecker.js
@@ -2,7 +2,7 @@ const { EmbedBuilder, ActivityType } = require('discord.js')
 const axios = require('axios')
 const fs = require('fs')
 const path = require('path')
-const { newTracksChannel } = require('../../../config.json')
+const { newTracksChannel } = require('../config.json');
 
 function generateDifficultyBar(difficulty) {
   const filledSquare = '▰'
